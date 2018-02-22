2cfc38eff029b23f8955d7c580dbbefefff350c8
Ashutosh Singh <ashutosh.singh@ubitricity.com>
2018-02-22 17:22:25 +0100

    no message

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ if [ -f ./prometheus/prometheus.yml ]; then
 fi
 
 . ./.env
-envsubst < "./prometheus/prometheus-template.yml" > "./prometheus/prometheus.yml"
+envsubst < "./prometheus/prometheus_template.yml" > "./prometheus/prometheus.yml"
 docker-compose up -d
 docker-compose ps
