3279da4625c486368d859a3d49a07e240c928c42
Ashutosh Singh <ashutosh.singh@ubitricity.com>
2018-02-22 17:18:51 +0100

    using posix standard

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ if [ -f ./prometheus/prometheus.yml ]; then
   rm -f ./prometheus/prometheus.yml
 fi
 
-source ./.env
+. ./.env
 envsubst < "./prometheus/prometheus-template.yml" > "./prometheus/prometheus.yml"
 docker-compose up -d
 docker-compose ps
