2a8e6c57392b994d009de2827015775677fda7ae
Ashutosh Singh <ashutosh.singh@smava.de>
2018-06-20 14:47:09 +0200

    second script exporter added

---
+++ scriptexporter/collector.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+EUREKA_URL='http://213.183.195.222:8761/eureka/apps'
+APPLICATIONS=$(curl -k -s $EUREKA_URL | grep name | grep -v MyOwn | awk -F '[<>]' '{print $3}')
+for app in $APPLICATIONS; do
+  app_status_page_urls=$(curl -k -s ${EUREKA_URL}/${app} | grep statusPageUrl | awk -F '[<>]' '{print $3}' )
+  for url in ${app_status_page_urls}; do
+    http_status_code=$(curl -s -o /dev/null -I -w "%{http_code}" $url)
+    echo "scriptexporter_http_status{app=\"$app\",url=\"$url\"} $http_status_code $(date +%s%3N)"
+  done
+done
